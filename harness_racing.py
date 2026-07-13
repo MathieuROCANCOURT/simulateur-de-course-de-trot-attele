2de8d8002217691894e648ab102ee5cbeb0bdc97
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-07-13 13:12:30 +0200

    feat: Initialize the dictionary for the race

--- harness_racing.py
+++ harness_racing.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+ARRAY_SPEED_DICE = (
+    (0, 1, 1, 1, 2, 2),
+    (0, 0, 1, 1, 1, 2),
+    (0, 0, 1, 1, 1, 2),
+    (-1, 0, 0, 1, 1, 1),
+    (-1, 0, 0, 0, 1, 1),
+    (-2, -1, 0, 0, 0, 1),
+    (-2, -1, 0, 0, 0, "D")
+)
+ARRAY_SPEED_DISTANCE = (0, 23, 46, 69, 92, 115, 138)
+
+
+def init_harness_racing(nb_horses):
+    return dict.fromkeys(nb_horses, 0)
+
+
 def print_ranking(ranking, type_rank):
     output, dict_type = "", {"tiercé": 3, "quarté": 4, "quinté": 5}
     top_ranking = dict_type[type_rank]
